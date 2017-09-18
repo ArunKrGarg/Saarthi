32419b66fa0d536cecc0de6d83e65de9bd51a997
Wiktor <wiktorkalinowski@gmail.com>
2017-09-18 16:38:42 +0200

    GET, POST Mapping

---
+++ src/main/java/com/example/demo/TopicService.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("Spring", "Spring Framework", "Spring Framework Description")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+}
