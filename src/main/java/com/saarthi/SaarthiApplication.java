3676bdc37ede5b49bec2b2d86fb3f8b01b6f63d5
arunkmrgarg <arunkmrgarg@gmail.com>
2020-04-30 11:01:56 +0530

    Disabling swagger

--- src/main/java/com/saarthi/SaarthiApplication.java
+++ src/main/java/com/saarthi/SaarthiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
 public class SaarthiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaarthiApplication.class, args);
--- src/main/java/com/saarthi/lender/BankLoan.java
+++ src/main/java/com/saarthi/lender/BankLoan.java
@@ -2,9 +2,13 @@ package com.saarthi.lender;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Embeddable;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.saarthi.lender.utils.BankName;
+import com.saarthi.utils.ListToJsonConverter;
 
 import lombok.Data;
 
@@ -14,5 +18,6 @@ public class BankLoan {
 	private BankName bank;
 	private int rate;
 	private String penalty;
+	@Convert(converter = ListToJsonConverter.class)
 	private List<String> termsAndConditions; 
 }
